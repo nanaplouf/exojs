ca87c8497e97cc07faf0036c880cc81cca860461
nanaplouf <vera78ds@gmail.com>
2025-06-24 10:25:59 +0200

    correction exo 1 + ajout exo 2 3 4

---
+++ assets/js/01-bonjour.js
@@ -0,0 +1,10 @@
+console.log('coucou');
+
+//Je crée deux variables;
+let nom;
+let prenom;
+
+nom = prompt("Entrez votre nom : ");
+prenom = prompt("Entrez votre prénom : ");
+
+alert(nom + " " + prenom);
